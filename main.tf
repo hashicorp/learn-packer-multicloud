8e6c3b8e4ee7bef468125d846a66d69b2242e03a
Tu Nguyen <im2nguyen@gmail.com>
2022-04-28 15:33:14 -0700

    separate terraform block

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.2.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {
     resource_group {
