11c69418886d33c5cdf049ddeae1b0c1ccd45470
gcastill0 <gilberto@hashicorp.com>
2022-05-17 19:45:30 -0400

    Optional sequence to avoid default password in DB

--- setup-deps-hashicups.sh
+++ setup-deps-hashicups.sh
@@ -14,5 +14,14 @@ git clone https://github.com/hashicorp-demoapp/hashicups-setups
 cd hashicups-setups/docker-compose-deployment
 git checkout server
 
+# Changing the password for DB to avoid exposing the default 
+# in the config files. This works when we don't need to know 
+# the password during a demo.
+
+ RANDOM_PASSWORD=$(openssl rand -hex 20)
+ sudo sed -i "s/=password/=$RANDOM_PASSWORD/g" docker-compose.yaml
+ sudo sed -i "s/=password/=$RANDOM_PASSWORD/g" conf.json
+ unset RANDOM_PASSWORD
+
 # Start HashiCups in background
 sudo docker-compose up -d
